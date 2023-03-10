2666d6ed6e27480dac98cf1a89ae891b0b641cc6
gkapelyushok <kapgosha@gmail.com>
2023-03-10 14:38:21 +0300

    Small changes in ArraySet.java 2

--- java-solutions/info/kgeorgiy/ja/kapelyushok/arrayset/ArraySet.java
+++ java-solutions/info/kgeorgiy/ja/kapelyushok/arrayset/ArraySet.java
@@ -93,6 +93,36 @@ public class ArraySet<E extends Comparable<E>> extends AbstractSet<E> implements
         return data.isEmpty();
     }
 
+    @Override
+    public boolean add(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
     private int binarySearch(E element) {
         return Collections.binarySearch(data, element, comparator());
     }
